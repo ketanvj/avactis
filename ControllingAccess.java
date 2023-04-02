ca78d1995ddd7ae38a09b8515959b71165fc1198
Ketan Jobanputra <ketanvj@yahoo.com>
2023-04-02 13:27:30 +0530

    Added a new file while in Dev branch

---
+++ ControllingAccess.java
@@ -0,0 +1,49 @@
+package d_classDemo;
+
+public class ControllingAccess {
+	 //Applicable to variables as well as methods
+	
+	/*            (P_One)  (P_One)       (P_Two)   (P_Two)
+	Modifier 		Alpha 	Beta     	AlphaSub 	Gamma
+	public 			Y 		Y 			Y 			Y           //i
+	protected 		Y 		Y 			Y		 	N           //j
+	no modifier 	Y 		Y 			N 			N           //l
+	private 		Y 		N 			N 			N           //k
+	 */
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*            (P_One)  (P_One)       (P_Two)   (P_Two)
+Modifier 		ClassA 	ClassB     	ClassC 	   ClassD
+public 			Y 		Y 			Y 			Y           //l
+protected 		Y 		Y 			Y		 	N           //l
+no modifier 	Y 		Y 			N 			N           //j
+private 		Y 		N 			N 			N           //i
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//protected class Protected {}
