e746f6623e9f7ea571380c99b4318a4214a8c80a
Ketan Jobanputra <ketanvj@yahoo.com>
2023-04-02 13:35:18 +0530

    New file in main branch

---
+++ TestCylinder.java
@@ -0,0 +1,16 @@
+package d_classDemo;
+
+public class TestCylinder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cylinder cy1 = new Cylinder(4,"Black",7);
+		Cylinder cy2 = new Cylinder();
+		
+		System.out.println("Area of Cylinder1 is - " + cy1.calculateArea());
+		System.out.println("Volume of Cylinder1 is - " + cy1.calculateVolume());
+		System.out.println("Volume of Cylinder1 is - " + cy1);
+		System.out.println("Volume of Cylinder1 is - " + cy1.toString());
+	}
+
+}
